37bd705f39259e7e6e992833f478fefb607da6b2
Sai Haridass <sharidass1408@gmail.com>
2018-12-15 17:29:32 -0500

    Code to merge elements of B into A when A is guranteed to have space for all element of B

--- src/com/chapter11/ArrayInsert_Pg360.java
+++ src/com/chapter11/ArrayInsert_Pg360.java
@@ -17,37 +17,71 @@ public class ArrayInsert_Pg360 {
 			System.out.println(x);
 		}
 		
-		unifyArrays(a,b);
-		
-		System.out.println("After Unification values of array a");
-		
-		for(int x: a) {
-			System.out.println(x);
+		if(unifyArrays(a,b)) {
+				
+			System.out.println("After Unification values of array a");
+			
+			for(int x: a) {
+				System.out.println(x);
+			}
+			
+		} else {
+			System.out.println(" Failed to Unify");
 		}
 	
 	}
 
-	private static void unifyArrays(int[] a, int[] b) {
+	private static boolean unifyArrays(int[] a, int[] b) {
 		int aLastIndex = getLastIndex(a);
 		int bLastIndex = getLastIndex(b);
 		
-		if(aLastIndex > -1 || bLastIndex > -1) {
-			return;
+		if(aLastIndex == -1 || bLastIndex == -1) {
+			return false;
 		}else {
+			//int mergedLastIndex = a.length -1; //this will leave gaps
 			
+			int mergedLastIndex = aLastIndex + bLastIndex + 1;
+			
+			//To insert elements from both a and b from their last locations
+			while(aLastIndex>=0 && bLastIndex>=0) {
+				if(a[aLastIndex] > b[bLastIndex]) {
+					a[mergedLastIndex] = a[aLastIndex];
+					mergedLastIndex--;
+					aLastIndex--;
+				}else {
+					a[mergedLastIndex] = b[aLastIndex];
+					mergedLastIndex--;
+					bLastIndex--;
+				}
+			}
+			
+			//To insert remaining elements from b into Merged Index.
+			while(bLastIndex > 0) {
+				a[mergedLastIndex] = b[bLastIndex];
+				mergedLastIndex--;
+				bLastIndex--;
+			}
+			return true;
 		}
 		
 	}
 
 	//Since this is a sorted array we return when element in higher idex has lower value than it previous
-	private static int getLastIndex(int[] a) {
+	private static int getLastIndex(int[] array) {
 		// TODO Auto-generated method stub
-		for(int i=0; i<= a.length-1; i++ ) {
+		for(int i=1; i<= array.length-1; i++ ) {
 			
-			if(a[i] > a[i+1])
-				return i;
+			if(array[i-1] > array[i]) 
+			{
+				if(array[i] == 0)
+					return i-1;
+				else
+					return -1;
+
+			}		
+				
 		}
-		return -1;
+		return array.length-1;
 	}
 
 	
