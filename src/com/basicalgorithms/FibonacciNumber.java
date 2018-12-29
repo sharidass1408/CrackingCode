f922e2158b5925a0f9ab7c8b82aab3db923962c0
Sai Haridass <sharidass1408@gmail.com>
2018-12-29 13:19:02 -0500

    FibonacciNumber

---
+++ src/com/basicalgorithms/FibonacciNumber.java
@@ -0,0 +1,26 @@
+package com.basicalgorithms;
+
+import java.util.Scanner;
+
+public class FibonacciNumber {
+
+	public static void main(String[] args) {
+
+		System.out.println("Please enter a valid number: ");
+		Scanner sc = new Scanner(System.in);
+		int value = sc.nextInt();
+		
+		System.out.println("Fibonacci value for the number is: " + fibonacci(value));
+		
+	}
+
+	private static int fibonacci(int n) {
+		
+		if(n <= 1)
+			return n;
+		else
+			return fibonacci(n-2) + fibonacci(n-1);
+		
+	}
+
+}
