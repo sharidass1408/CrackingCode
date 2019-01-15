e25238241e520f43bd4758a49a6b33fcb55f7d87
Sai Haridass <sharidass1408@gmail.com>
2019-01-15 17:41:25 -0500

    Sorting stack with only one more stack

---
+++ src/com/chapter3/pg215/SortingStackWithJustOneMoreStack.java
@@ -0,0 +1,28 @@
+package com.chapter3.pg215;
+
+import java.util.Stack;
+
+public class SortingStackWithJustOneMoreStack<T> {
+	
+
+	public Stack<Integer> sortStack(Stack<Integer> originalStack){
+		
+		Stack<Integer> sortedStack = new Stack<>();
+		
+		while( !originalStack.isEmpty()) {
+			int temp = originalStack.pop();
+			
+			
+			while(!sortedStack.empty() && sortedStack.peek() > temp) {
+				originalStack.push(sortedStack.pop());
+			}
+			
+			/*We get to the below line only when sortedStack is empty 
+			       or
+			if sorted stack hold value less temp */
+			sortedStack.push(temp);
+		}
+
+		return sortedStack;
+	}
+}
---
+++ src/com/chapter3/pg215/SortingStackWithJustOneMoreStackTest.java
@@ -0,0 +1,35 @@
+package com.chapter3.pg215;
+
+import java.util.Stack;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.hamcrest.collection.IsIterableContainingInOrder;
+
+public class SortingStackWithJustOneMoreStackTest {
+
+	@Test
+	public void sort_4Integers_Sorted4Integers() {
+		
+		Stack<Integer> originalStack = new Stack<>();
+		
+		originalStack.push(2);
+		originalStack.push(1);
+		originalStack.push(3);
+		originalStack.push(4);
+		
+		Stack<Integer> expectedStack = new Stack<>();
+		
+		expectedStack.push(1);
+		expectedStack.push(2);
+		expectedStack.push(3);
+		expectedStack.push(4);
+		
+		SortingStackWithJustOneMoreStack sortStack = new SortingStackWithJustOneMoreStack();
+		Stack<Integer> sortedStack = sortStack.sortStack(originalStack);
+		
+		Assert.assertThat(expectedStack, 
+			       IsIterableContainingInOrder.contains(originalStack.toArray()));
+		
+	}
+}
