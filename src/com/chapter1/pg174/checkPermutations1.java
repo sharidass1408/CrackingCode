04fb76733194fd8c53a09c02865e88082545c1d3
Sai Haridass <sharidass1408@gmail.com>
2020-01-12 07:31:32 -0500

    Adding additional test cases

--- src/com/chapter1/pg174/checkPermutations1.java
+++ src/com/chapter1/pg174/checkPermutations1.java
@@ -7,8 +7,10 @@ public class checkPermutations1 {
 	public static void main(String[] args) {
 		String str1 = "dog";
 		String str2 = "god";
+		String str3 = "cat";
 		
 		System.out.println("String are permutations of each other: " + permutation(str1, str2));
+		System.out.println("String are permutations of each other: " + permutation(str1, str3));
 	}
 
 	private static boolean permutation(String str1, String str2) {
