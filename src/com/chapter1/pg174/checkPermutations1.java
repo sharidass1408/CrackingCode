3ecb72e6a21707956269ab12969344ab035c9dc8
Sai Haridass <sharidass1408@gmail.com>
2020-01-12 08:23:28 -0500

    More test data

--- src/com/chapter1/pg174/checkPermutations1.java
+++ src/com/chapter1/pg174/checkPermutations1.java
@@ -7,16 +7,23 @@ public class checkPermutations1 {
 	public static void main(String[] args) {
 		String str1 = "dog";
 		String str2 = "god";
-		String str3 = "cat";
+		String str3 = "caat";
+		String str4 = "atac";
+		String str5 = "c  aat";
+		String str6 = "caa  t";
 		
 		System.out.println("String are permutations of each other: " + permutation(str1, str2));
 		System.out.println("String are permutations of each other: " + permutation(str1, str3));
+		System.out.println("String are permutations of each other: " + permutation(str3, str4));
+		System.out.println("String are permutations of each other: " + permutation(str3, str5));
+		System.out.println("String are permutations of each other: " + permutation(str5, str6));
 	}
 
 	private static boolean permutation(String str1, String str2) {
 
-		return true;
 		
+		return false;
+						
 	}
 
 }
