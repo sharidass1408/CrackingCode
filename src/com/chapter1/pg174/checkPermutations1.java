ea47504cb5e0f4103bea90cfa74b3d27e78c0ac4
Sai Haridass <sharidass1408@gmail.com>
2020-01-12 07:04:24 -0500

    Permutation

--- src/com/chapter1/pg174/checkPermutations1.java
+++ src/com/chapter1/pg174/checkPermutations1.java
@@ -12,22 +12,7 @@ public class checkPermutations1 {
 	}
 
 	private static boolean permutation(String str1, String str2) {
-		
-		if(str1.length() != str2.length() )
-			return false;
-		
-		char[] str1Char = str1.toCharArray();
-		Arrays.sort(str1Char);
-		
-		char[] str2Char = str2.toCharArray();
-		Arrays.sort(str2Char);
-		
-		for(int i = 0; i < str1Char.length; i++) {
-			
-			if(str1Char[i] != str2Char[i])
-				return false;
-				
-		}
+
 		return true;
 		
 	}
